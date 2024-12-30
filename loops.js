8e5c471ff3c46a1e34ba9e9685bb0e425fcbe540
FaisalJadallah <faisal.jadallah.fj@gmail.com>
2024-12-30 16:42:37 +0300

    branch

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-// //Q1
+//Q1
 let userName = prompt("Enter your Name please :");
 let Gender;
 
@@ -19,36 +19,65 @@ if (Gender === "male") {
 let orderChoice = prompt("Would you like to order shawarma, zinger, or burger");
 alert(`Your ${orderChoice} is being prepared.`);
 console.log(`${userName} ordered ${orderChoice}`)
+
+//ex3
+//q1
+let y=function (x){
+    let num;
+    for(i=x.length;i>=0;i--){
+    num+=x[i];
+    }
+    return num;
+    }
+    let x="532443";
+    console.log(y(x));
+    
+    
+    //q2
+    for(i=0;i<=15;i++){
+         if(i%2==0){
+            console.log(i+" is even");
+        }
+        else if(i%2!=0){
+            console.log(i+" is odd");
+        }
+    }
+
+
 //Q1
-// for (let i = 0; i <= 5; i++) {
-//     alert(i);
-// }
-
-// //Q2
-// let number;
-
-// while (true) {
-//     number = prompt("Please enter a number between 0 and 100:");
-
-//     if (number >= 0 && number <= 100) {
-//         break;
-//     } else {
-//         alert("Invalid input. Please enter a number between 0 and 100.");
-//     }
-// }
+for (let i = 0; i <= 5; i++) {
+    alert(i);
+}
 
 
-// alert(`Thank you! You entered a valid number: ${number}`);
+//Q2
+let number;
 
-// //Q3
+while (true) {
+    number = prompt("Please enter a number between 0 and 100:");
 
-// let number2 = parseInt(prompt("Please enter an integer:"));
+    if (number >= 0 && number <= 100) {
+        break;
+    } else {
+        alert("Invalid input. Please enter a number between 0 and 100.");
+    }
+}
 
-// let sum = 0;
+
+alert(`Thank you! You entered a valid number: ${number}`);
+
+//Q3
+
+let number2 = parseInt(prompt("Please enter an integer:"));
+
+let sum = 0;
 
 
-// for (let i = 0; i <= number2; i++) {
-//   sum += i;
-// }
+for (let i = 0; i <= number2; i++) {
+  sum += i;
+}
 
-// alert(`The sum of integers from 0 to ${number2} is: ${sum}`);
+alert(`The sum of integers from 0 to ${number2} is: ${sum}`);
+
+
+
